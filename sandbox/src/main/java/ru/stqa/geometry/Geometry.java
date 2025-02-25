94a74dcc3ae2f46c4713993557dbe8c74244ea88
minchenko <minchenko.kl@yandex.ru>
2025-02-25 08:55:39 +0300

    Triangle_Class

--- sandbox/src/main/java/ru/stqa/geometry/Geometry.java
+++ sandbox/src/main/java/ru/stqa/geometry/Geometry.java
@@ -2,6 +2,7 @@ package ru.stqa.geometry;
 
 import ru.stqa.geometry.figures.Rectangle; //используем класс, находящийся в другом пакете
 import ru.stqa.geometry.figures.Square;
+import ru.stqa.geometry.figures.Triangle;
 
 public class Geometry {
     public static void main(String[] args) {
@@ -17,6 +18,9 @@ public class Geometry {
         Rectangle.printRectangleArea(3.0, 5.0);
         Rectangle.printRectangleArea(8.0, 9.0);
 
+        Triangle tr = new Triangle(5.0, 7.0, 8.0);
+        System.out.println(tr.calculatePerimeter());
+        System.out.println(tr.calculateSquare());
 
     }
 
---
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -0,0 +1,41 @@
+package ru.stqa.geometry.figures;
+
+public class Triangle {
+   private double a;
+   private double b;
+   private double c;
+
+   public Triangle(double a, double b, double c) {
+       this.a = a;
+       this.b = b;
+       this.c = c;
+   }
+
+   public double calculatePerimeter() {
+      return this.a + this.b + this.c;
+   }
+
+   public double calculateSquare() {
+       // Condition check over sides of triangle
+           if (this.a < 0 || this.b < 0 || this.c < 0 || (this.a + this.b <= this.c)
+                   || this.a + this.c <= this.b || this.b + this.c <= this.a)
+
+           // Length of sides must be positive and sum of
+           // any two sides must be smaller than third side
+           {
+               // print message if condition fails
+               System.out.println("Not a valid side value");
+               System.exit(0);
+           }
+
+           //else
+            double p = calculatePerimeter()/2;
+            return Math.sqrt(p*(p-this.a)*(p-this.b)*(p-this.c));
+   }
+/***
+    public static void printTriangleSquare(double a, double b, double c) {
+        var text = String.format("Площадь треугольника со сторонами %f, %f, %f = %f", a, b, c, calculateSquare());
+        System.out.println(text);
+    }
+***/
+}

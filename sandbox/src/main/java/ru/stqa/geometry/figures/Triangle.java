b701866c5ab3789b48477bf21e13f6d0b5269ab0
minchenko <minchenko.kl@yandex.ru>
2025-02-25 09:56:19 +0300

    Triangle_record

--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -1,15 +1,10 @@
 package ru.stqa.geometry.figures;
 
-public class Triangle {
-   private double a;
-   private double b;
-   private double c;
-
-   public Triangle(double a, double b, double c) {
-       this.a = a;
-       this.b = b;
-       this.c = c;
-   }
+public record Triangle (
+        double a,
+        double b,
+        double c
+) {
 
    public double calculatePerimeter() {
       return this.a + this.b + this.c;

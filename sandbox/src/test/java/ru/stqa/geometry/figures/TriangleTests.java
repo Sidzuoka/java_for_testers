e5001681ec53b899d8e51d6191cbd4aa29d2a70f
minchenko <minchenko.kl@yandex.ru>
2025-03-03 08:40:36 +0300

    Triangle_throw_Constructor_2Test_3_ver2

--- sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -44,7 +44,10 @@ public class TriangleTests {
             var tr1 = new Triangle(5.0, 6.0, 7.0);
             //var tr2 = new Triangle(5.0, 6.0, 7.0);
             //var tr2 = new Triangle(7.0, 5.0, 6.0);
-            var tr2 = new Triangle(6.0, 7.0, 5.0);
+            //var tr2 = new Triangle(6.0, 7.0, 5.0);
+            //var tr2 = new Triangle(5.0, 7.0, 6.0);
+            //var tr2 = new Triangle(6.0, 5.0, 7.0);
+            var tr2 = new Triangle(7.0, 6.0, 5.0);
             Assertions.assertTrue(tr1.equals(tr2));
         }
 

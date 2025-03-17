8e272eab2c13e1e6998c9d5b65ca11b665ed26fc
minchenko <minchenko.kl@yandex.ru>
2025-03-17 15:18:53 +0300

    ParameterizedTest_NameOnly_4.5.2

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -15,24 +15,14 @@ import java.util.List;
 public class GroupCreationTests extends TestBase {
 
     public static List<String> groupNameProvider() {
-        var result = new ArrayList<String>();
+        //7 групп создаст
+        //2 - фиксированные значения - group name, group name'
+        //5 - циклом сгенерированные со случайными значениями
+        var result = new ArrayList<String>(List.of("group name", "group name'"));
         for (int i = 0; i < 5; i++ ){
             result.add(randomString(i * 10));
         }
         return result;
-
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"group name", "group name'"})
-    //имя группы будет параметром
-    public void canCreateGroup(String name) {
-        //посчитать кол-во групп до выполнения операции
-        int groupCount = app.groups().getCount();
-        app.groups().createGroup(new GroupData(name, "group header", "group footer"));
-        //после операции получаем новое значение
-        int newGroupCount = app.groups().getCount();
-        Assertions.assertEquals(groupCount + 1, newGroupCount);
 
     }
 
@@ -52,7 +42,7 @@ public class GroupCreationTests extends TestBase {
     public void canCreateMultipleGroups(String name) {
         //посчитать кол-во групп до выполнения операции
         int groupCount = app.groups().getCount();
-        app.groups().createGroup(new GroupData( name, "group header", "group footer"));
+        app.groups().createGroup(new GroupData(name, "group header", "group footer"));
         //после операции получаем новое значение
         int newGroupCount = app.groups().getCount();
         Assertions.assertEquals(groupCount + 1, newGroupCount);

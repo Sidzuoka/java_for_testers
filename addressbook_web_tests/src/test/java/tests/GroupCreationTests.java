a06ed68079ee0272c9086bc15ad911cd20166128
minchenko <minchenko.kl@yandex.ru>
2025-03-18 13:00:41 +0300

    ParameterizedTest_PositiveNegative_4.5.5

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -2,7 +2,6 @@ package tests;
 
 import model.GroupData;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -14,19 +13,12 @@ import java.util.List;
 public class GroupCreationTests extends TestBase {
 
     public static List<GroupData> groupProvider() {
-        //7 групп создаст
-        //2 - фиксированные значения - group name, group name'
+        //13 групп создаст
+        //8 - комбинации 4*2 из вложенных циклов
         //5 - циклом сгенерированные со случайными значениями
-        var result = new ArrayList<GroupData>(List.of(
-                new GroupData(),
-                new GroupData().withName("new name group"),
-                new GroupData("group name", "", ""),
-                new GroupData("group name'", "", "")));
-        for (int i = 0; i < 5; i++ ){
-            result.add(new GroupData(randomString(i * 10), randomString(i * 10), randomString(i * 10)));
-        }
-        return result;
-       /*
+
+        var result = new ArrayList<GroupData>();
+
         // перебирает 2 значения name - пустой и непустой
         for (var name : List.of("", "group name")) {
             // перебирает 2 возможных значения header - пустой и непустой
@@ -37,9 +29,11 @@ public class GroupCreationTests extends TestBase {
                 }
             }
         }
-
-        */
-
+        for (int i = 0; i < 5; i++ ){
+            //длина строки i=0 -> входные данные могут дублироваться, т.к. рандомно подбираются
+            result.add(new GroupData(randomString(i * 10), randomString(i * 10), randomString(i * 10)));
+        }
+        return result;
 
     }
 
@@ -53,7 +47,25 @@ public class GroupCreationTests extends TestBase {
         //после операции получаем новое значение
         int newGroupCount = app.groups().getCount();
         Assertions.assertEquals(groupCount + 1, newGroupCount);
+    }
 
+
+    public static List<GroupData> negativeGroupProvider() {
+        //1 - негативный - name' - когда не можем создать группу
+        var result = new ArrayList<GroupData>(List.of(new GroupData("group name'", "", "")));
+        return result;
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("negativeGroupProvider")
+    public void canNotCreateGroup(GroupData group) {
+        //посчитать кол-во групп до выполнения операции
+        int groupCount = app.groups().getCount();
+        app.groups().createGroup(group);
+        //после операции получаем новое значение
+        int newGroupCount = app.groups().getCount();
+        Assertions.assertEquals(groupCount, newGroupCount);
     }
 
 

25be7b32155c585ade5944afdc19ab754463d6cf
minchenko <minchenko.kl@yandex.ru>
2025-03-19 15:42:53 +0300

    ParameterizedTest_AddressPositiveNegative_4.5

--- addressbook_web_tests/src/test/java/tests/AddressCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/AddressCreationTests.java
@@ -2,7 +2,6 @@ package tests;
 
 import model.AddressData;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import java.util.ArrayList;
@@ -12,19 +11,32 @@ public class AddressCreationTests extends TestBase{
 
 
     public static List<AddressData> addressProvider() {
-        var result = new ArrayList<AddressData>(List.of(
-                new AddressData(),
-                new AddressData().withFirstName("OneField - FirstName"),
-                new AddressData("FirstName", "", "", "",""),
-                new AddressData("FirstName'", "", "", "", "")));
-
+        var result = new ArrayList<AddressData>();
+        for (var firstName : List.of("", "firstName")) {
+            for (var lastName : List.of("", "lastName")) {
+                for (var address : List.of("", "address")) {
+                    for (var home : List.of("", "home")) {
+                        for (var email: List.of("", "email")){
+                            result.add(new AddressData(firstName, lastName, address, home, email));
+                        }
+                    }
+                }
+            }
+        }
         for (int i = 0; i < 5; i++) {
             result.add(new AddressData(randomString(i * 10), randomString(i * 10),
                     randomString(i * 10), randomString(i * 10), randomString(i * 10)));
         }
         return result;
     }
-    
+
+
+    public static List<AddressData> negativeAddressProvider() {
+        var result = new ArrayList<AddressData>(List.of(
+                new AddressData("FirstName'", "", "", "", "")));
+        return result;
+    }
+
 
     @ParameterizedTest
     @MethodSource("addressProvider")
@@ -33,6 +45,15 @@ public class AddressCreationTests extends TestBase{
         app.address().createAddress(address);
         int newAddressCount = app.address().getCountAddress();
         Assertions.assertEquals(addressCount + 1, newAddressCount);
+    }
+
+    @ParameterizedTest
+    @MethodSource("negativeAddressProvider")
+    public void canNotCreateAddress(AddressData address) {
+        int addressCount = app.address().getCountAddress();
+        app.address().createAddress(address);
+        int newAddressCount = app.address().getCountAddress();
+        Assertions.assertEquals(addressCount, newAddressCount);
     }
 
 }

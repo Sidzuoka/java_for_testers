6187391e71b831b3f2e592c082a0d6945a47c08c
minchenko <minchenko.kl@yandex.ru>
2025-03-19 15:15:31 +0300

    ParameterizedTest_DataDrivenTesting_4.5

--- addressbook_web_tests/src/test/java/tests/AddressCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/AddressCreationTests.java
@@ -5,54 +5,34 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AddressCreationTests extends TestBase{
 
 
-    public static List<String> addressFirstNameProvider() {
-        var result = new ArrayList<String>();
+    public static List<AddressData> addressProvider() {
+        var result = new ArrayList<AddressData>(List.of(
+                new AddressData(),
+                new AddressData().withFirstName("OneField - FirstName"),
+                new AddressData("FirstName", "", "", "",""),
+                new AddressData("FirstName'", "", "", "", "")));
 
         for (int i = 0; i < 5; i++) {
-            result.add(randomString(i * 10));
+            result.add(new AddressData(randomString(i * 10), randomString(i * 10),
+                    randomString(i * 10), randomString(i * 10), randomString(i * 10)));
         }
         return result;
     }
+    
 
     @ParameterizedTest
-    @ValueSource(strings = {"FirstName", "FirstName'"})
-    public void canCreateAddress(String firstName) {
+    @MethodSource("addressProvider")
+    public void canCreateMultipleAddress(AddressData address) {
         int addressCount = app.address().getCountAddress();
-        app.address().createAddress( new AddressData(firstName, "Lastname",
-                "Address", "HomeTelephone", "email"));
+        app.address().createAddress(address);
         int newAddressCount = app.address().getCountAddress();
         Assertions.assertEquals(addressCount + 1, newAddressCount);
-    }
-
-    @Test
-    public void canCreateAddressWithEmptyField() {
-        app.address().createAddress(new AddressData());
-    }
-
-    @Test
-    public void canCreateAddressWithFirstNameOnly() {
-        app.address().createAddress(new AddressData().withFirstName("OneField - FirstName"));
-    }
-
-    @ParameterizedTest
-    @MethodSource("addressFirstNameProvider")
-    public void canCreateMultipleAddress(String firstName) {
-
-        int addressCount = app.address().getCountAddress();
-
-        app.address().createAddress(new AddressData(firstName, "Lastname",
-                    "Address", "HomeTelephone", "email"));
-
-
-        int newAddressCount = app.address().getCountAddress();
-        Assertions.assertEquals(addressCount + n, newAddressCount);
     }
 
 }

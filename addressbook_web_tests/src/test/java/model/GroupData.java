5f857aec4351d3de2691d230727c98888b652f48
minchenko <minchenko.kl@yandex.ru>
2025-03-21 08:28:46 +0300

    ParameterizedTest_HomeWork_ErrDiscard_4.5

--- addressbook_web_tests/src/test/java/model/GroupData.java
+++ addressbook_web_tests/src/test/java/model/GroupData.java
@@ -16,11 +16,11 @@ public record GroupData(String id, String name, String header, String footer) {
         return new GroupData(id, this.name, this.header, this.footer);
     }
 
-    public GroupData withHeader(String name) {
+    public GroupData withHeader(String header) {
         return new GroupData(this.id, this.name, header, this.footer);
     }
 
-    public GroupData withFooter(String name) {
+    public GroupData withFooter(String footer) {
         return new GroupData(this.id, this.name, this.header, footer);
     }
 

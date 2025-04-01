e2d6af188680af8916efa73bb1ab5bd0576fca77
minchenko <minchenko.kl@yandex.ru>
2025-04-01 14:01:10 +0300

    ConfigFile_local.properties_5.8

--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -3,17 +3,27 @@ package tests;
 import manager.ApplicationManager;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class TestBase {
 
     protected static ApplicationManager app; //глобальная ссылка
 
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws IOException {
         if (app == null) {
+            var properties = new Properties();
+            properties.load(new FileReader(System.getProperty("target", "local.properties")));//.load - читаем данные из файла
+                                              // используем системное св-во для выбора нужного нам файла конфигурации при запуске теста
+                                              // target - целевая система, local.properties - если не указаны - то загружаем нстройки из конфиг. ф.
             app = new ApplicationManager();
+            app.init(System.getProperty("browser", "firefox"), properties);
         }
 
-        app.init(System.getProperty("browser", "firefox"));
+
     }
 
 }

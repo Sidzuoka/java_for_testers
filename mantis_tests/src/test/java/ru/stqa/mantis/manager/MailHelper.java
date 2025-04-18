6f6dd646bf2782fcaa1aa20508dce9ab8e4de077
minchenko <minchenko.kl@yandex.ru>
2025-04-18 11:25:31 +0300

    MailHelper_избавление от переменной_8.6.2

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/MailHelper.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/MailHelper.java
@@ -20,13 +20,7 @@ public class MailHelper extends HelperBase {
         var start = System.currentTimeMillis();
         while (System.currentTimeMillis() < start + duration.toMillis()) {
             try {
-                //создаем сессию для работы с почтовым сервером
-                var session = Session.getInstance(new Properties()); //пеердали пустой объект properties
-                //получаем доступ к хранилищу почты - устанавливаем соединение с сервером почтовым
-                Store store = session.getStore("pop3");
-                store.connect("localhost",username, password);
-                //открываем почтовый ящик
-                var inbox = store.getFolder("INBOX"); //почтовый ящик один, называется INBOX
+                var inbox = getInbox(username, password);
                 //открываем ящик
                 inbox.open(Folder.READ_ONLY); //только читаем почту, удалять ничего не будим (READ_WRITE)
                 var messages = inbox.getMessages(); //возвращается массив, его нужно преобразовать в список модельных объектов
@@ -43,7 +37,7 @@ public class MailHelper extends HelperBase {
                         })
                         .toList();
                 inbox.close();
-                store.close();
+                inbox.getStore().close(); //взяли переменную store из хранилища - избавились от нее, чтобы вынести метод с этой переменной
                 if (result.size() > 0) {
                     return result;
                 } //если список пустой, ничего не возвращаем и уходим на следующую итерацию цикла
@@ -59,8 +53,44 @@ public class MailHelper extends HelperBase {
         throw new RuntimeException("No mail");
     }
 
+    private static Folder getInbox(String username, String password) {
+        //создаем сессию для работы с почтовым сервером
+        var session = Session.getInstance(new Properties()); //пеердали пустой объект properties
+        try {
+            //получаем доступ к хранилищу почты - устанавливаем соединение с сервером почтовым
+            Store store = session.getStore("pop3");
+            store.connect("localhost", username, password);
+            //открываем почтовый ящик
+            var inbox = store.getFolder("INBOX"); //почтовый ящик один, называется INBOX
+            return inbox;
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
 
-    public void drain() {
+    }
+
+
+    public void drain(String username, String password) {
+        try {
+            //получили inbox
+            var inbox = getInbox(username, password);
+            //открываем inbox
+            inbox.open(Folder.READ_WRITE);
+            //массив
+            Arrays.stream(inbox.getMessages()).forEach(m -> {
+                try {
+                    m.setFlag(Flags.Flag.DELETED, true);
+                } catch (MessagingException e) {
+                    throw new RuntimeException(e);
+                }
+            });//setFlag - помечаем каждое письмо, как удаленное
+            //при закрытии inbox, все письма, помеченные как DELETED, будут удалены
+            inbox.close();
+            //закрываем хранилище
+            inbox.getStore().close();
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
--- mantis_tests/src/test/java/ru/stqa/mantis/tests/MailTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/MailTests.java
@@ -4,13 +4,36 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.util.regex.Pattern;
 
 public class MailTests extends TestBase {
 
+    //после отладки ф-ий, можно удалить
+    @Test
+    void canDrainInbox() {
+        app.mail().drain("user1@localhost", "password");
+    }
+
+    //после отладки ф-ий, можно удалить
     @Test
     void canReceiveEmail() {
-        var messages = app.mail().receive("user1@localhost", "password", Duration.ofSeconds(10));
+        var messages = app.mail().receive("user1@localhost", "password", Duration.ofSeconds(60));
         Assertions.assertEquals(1, messages.size());
         System.out.println(messages);
+    }
+
+    //как извлечь ссылку из текста письма
+    @Test
+    void canExtractUrl() {
+        //получаем почту
+        var messages = app.mail().receive("user1@localhost", "password", Duration.ofSeconds(60));
+        //берем текст первого письма, из кот. будим извлекать ссылку
+        var text = messages.get(0).content();
+        var pattern = Pattern.compile("http://\\S+"); // \\S - НЕ пробел --- создали шаблон
+        var matcher = pattern.matcher(text); // применили шаблон
+        if (matcher.find()) {
+            var url = text.substring(matcher.start(), matcher.end());
+            System.out.println(url);
+        }
     }
 }

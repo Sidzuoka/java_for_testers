6fa928ef59603b992f562fba354d3b70bf758964
minchenko <minchenko.kl@yandex.ru>
2025-04-18 10:51:27 +0300

    MailHelper_8.6.1

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/MailHelper.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/MailHelper.java
@@ -4,6 +4,7 @@ import jakarta.mail.*;
 import ru.stqa.mantis.model.MailMessage;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
@@ -14,36 +15,54 @@ public class MailHelper extends HelperBase {
         super(manager);
     }
 
-    public List<MailMessage> receive(String username, String password) {
+    public List<MailMessage> receive(String username, String password, Duration duration) {
+        //ждем проверяем почту, ждем, проверяем почту, совсем ничего нет выдаем исключение
+        var start = System.currentTimeMillis();
+        while (System.currentTimeMillis() < start + duration.toMillis()) {
+            try {
+                //создаем сессию для работы с почтовым сервером
+                var session = Session.getInstance(new Properties()); //пеердали пустой объект properties
+                //получаем доступ к хранилищу почты - устанавливаем соединение с сервером почтовым
+                Store store = session.getStore("pop3");
+                store.connect("localhost",username, password);
+                //открываем почтовый ящик
+                var inbox = store.getFolder("INBOX"); //почтовый ящик один, называется INBOX
+                //открываем ящик
+                inbox.open(Folder.READ_ONLY); //только читаем почту, удалять ничего не будим (READ_WRITE)
+                var messages = inbox.getMessages(); //возвращается массив, его нужно преобразовать в список модельных объектов
+                //массив преобразовываем в модельные объекты
+                var result =  Arrays.stream(messages)
+                        .map(m -> {
+                            try {
+                                return new MailMessage()
+                                        .withFrom(m.getFrom()[0].toString())
+                                        .withContent((String) m.getContent()); //(String) m.getContent() - приводим к типу String
+                            } catch (MessagingException | IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        })
+                        .toList();
+                inbox.close();
+                store.close();
+                if (result.size() > 0) {
+                    return result;
+                } //если список пустой, ничего не возвращаем и уходим на следующую итерацию цикла
+            } catch (MessagingException e) {
+                throw new RuntimeException(e);
+            }
+        }
         try {
-            //создаем сессию для работы с почтовым сервером
-            var session = Session.getInstance(new Properties()); //пеердали пустой объект properties
-            //получаем доступ к хранилищу почты - устанавливаем соединение с сервером почтовым
-            Store store = session.getStore("pop3");
-            store.connect("localhost",username, password);
-            //открываем почтовый ящик
-            var inbox = store.getFolder("INBOX"); //почтовый ящик один, называется INBOX
-            //открываем ящик
-            inbox.open(Folder.READ_ONLY); //только читаем почту, удалять ничего не будим (READ_WRITE)
-            var messages = inbox.getMessages(); //возвращается массив, его нужно преобразовать в список модельных объектов
-            //массив преобразовываем в модельные объекты
-            var result =  Arrays.stream(messages)
-                    .map(m -> {
-                        try {
-                            return new MailMessage()
-                                    .withFrom(m.getFrom()[0].toString())
-                                    .withContent((String) m.getContent()); //(String) m.getContent() - приводим к типу String
-                        } catch (MessagingException | IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                    })
-                    .toList();
-            inbox.close();
-            store.close();
-            return result;
-        } catch (MessagingException e) {
+            Thread.sleep(1000); // 1 сек.
+        } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+        throw new RuntimeException("No mail");
+    }
+
+
+    public void drain() {
 
     }
+
+
 }
--- mantis_tests/src/test/java/ru/stqa/mantis/tests/MailTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/MailTests.java
@@ -3,11 +3,13 @@ package ru.stqa.mantis.tests;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 public class MailTests extends TestBase {
 
     @Test
     void canReceiveEmail() {
-        var messages = app.mail().receive("user1@localhost", "password");
+        var messages = app.mail().receive("user1@localhost", "password", Duration.ofSeconds(10));
         Assertions.assertEquals(1, messages.size());
         System.out.println(messages);
     }

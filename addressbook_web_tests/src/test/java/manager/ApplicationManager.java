2df4b95bc8ffcb0d64e89b25cfd355ed17e0ef5b
minchenko <minchenko.kl@yandex.ru>
2025-04-25 14:39:38 +0300

    SeleniumServerGrid_10.5

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -5,8 +5,13 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 
 public class ApplicationManager {
@@ -27,13 +32,25 @@ public class ApplicationManager {
 
 
 
-    public void init(String browser, Properties properties) {
+    public void init(String browser, Properties properties) throws MalformedURLException {
         this.properties = properties;
         if (driver == null) {
+            var seleniumServer = properties.getProperty("seleniumServer");
             if ("firefox".equals(browser)) {
-                driver = new FirefoxDriver();
+                if(seleniumServer != null) {
+                    //запуск драйвера на удаленной машине
+                    driver = new RemoteWebDriver(new URL(seleniumServer), new FirefoxOptions());
+                            //адрес сервера, кот. нужно запустить; браузер, кот. нужно запустить
+                } else {
+                    //иначе запускаем браузер на локальной машине
+                    driver = new FirefoxDriver();
+                }
             } else if ("chrome".equals(browser)) {
-                driver = new ChromeDriver();
+                if (seleniumServer != null) {
+                    driver = new RemoteWebDriver(new URL(seleniumServer), new ChromeOptions());
+                } else {
+                    driver = new ChromeDriver();
+                }
             } else {
                 throw new IllegalArgumentException(String.format("Unknow browser %s", browser));
             }
